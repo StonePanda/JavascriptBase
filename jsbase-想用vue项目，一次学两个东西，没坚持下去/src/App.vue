008ee7872a0f7d1d85390efba43a540e9d5625b5
StonePanda <2504271266@qq.com>
2021-12-23 11:41:07 +0800

    没坚持下去的html和vue项目

---
+++ jsbase-想用vue项目，一次学两个东西，没坚持下去/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <code-display></code-display>
+</template>
+
+<script>
+import CodeDisplay from './components/CodeDisplay.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CodeDisplay
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ jsbase-想用vue项目，一次学两个东西，没坚持下去/src/components/CodeDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>{{operation}}</h3>
+        <textarea rows="4" readonly>
+            let x=2
+            let y=3
+            x/=y
+            console.log(x,y) //注意js里没有整除这个概念
+        </textarea>
+        <br>
+        <button @click="x/=y">除法赋值结果</button>
+        <br>
+        <textarea rows="1" readonly>
+            <!-- {{x}} {{y}} -->
+        </textarea>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
